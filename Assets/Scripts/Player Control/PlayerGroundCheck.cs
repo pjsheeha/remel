ab908e6c278d8d952eeeb7e0fbaba047a91d3dba
Kyle Xiong <kxiong@cs.cmu.edu>
2017-09-29 18:03:16 -0400

    Basic movement (left, right, jumps) with a Kirby sprite. Triggers animations using walk (bool), jump (trigger), air_jump(trigger), grounded (bool) conditions.

---
+++ Assets/Scripts/Player Control/PlayerGroundCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerMovement))]
+
+public class PlayerGroundCheck : MonoBehaviour {
+
+	public Vector3 verticalVelocitySeries;
+
+	private Rigidbody2D rb;
+	private PlayerMovement playerMovement;
+
+	public bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		playerMovement = GetComponent<PlayerMovement> ();
+		rb = GetComponent<Rigidbody2D> ();
+
+		verticalVelocitySeries = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GroundCheck ();
+
+		if (isGrounded) {
+			playerMovement.ResetJumps ();
+		}
+
+		playerMovement.anim.SetBool ("grounded", isGrounded);
+	}
+
+	private void GroundCheck() {
+		verticalVelocitySeries.x = verticalVelocitySeries.y;
+		verticalVelocitySeries.y = verticalVelocitySeries.z;
+
+		verticalVelocitySeries.z = rb.velocity.y;
+
+		isGrounded = verticalVelocitySeries.magnitude == 0f;
+	}
+}
---
+++ Assets/Scripts/Player Control/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Animator))]
+[RequireComponent (typeof(SpriteRenderer))]
+[RequireComponent (typeof(BoxCollider2D))]
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	protected float moveSpeed = 2.0f;
+	[SerializeField]
+	protected float jumpHeight = 4.0f;
+	[SerializeField]
+	protected int numJumps = 5;
+
+	public Animator anim;
+
+	private BoxCollider2D coll;
+	private PlayerGroundCheck playerGroundCheck;
+	private Rigidbody2D rb;
+	private SpriteRenderer sr;
+
+	private float h_Input;
+	private int remainingJumps;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		coll = GetComponent<BoxCollider2D> ();
+		playerGroundCheck = GetComponent<PlayerGroundCheck> ();
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+
+		remainingJumps = numJumps;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HorizontalMovement ();
+		Jump ();
+	}
+
+	private void HorizontalMovement() {
+		h_Input = Input.GetAxis ("Horizontal");
+
+		rb.transform.position += Vector3.right * h_Input * Time.deltaTime * moveSpeed;
+
+		anim.SetBool ("walk", Mathf.Abs(h_Input) > 0f);
+
+		sr.flipX = h_Input != 0f ? (h_Input > 0f ? false : true) : sr.flipX;
+	}
+
+	private void Jump() {
+		if (Input.GetKeyDown (KeyCode.UpArrow) && remainingJumps > 0) {
+			Vector2 vel = rb.velocity;
+			vel.y = jumpHeight * remainingJumps-- / numJumps;
+
+			rb.velocity = vel;
+
+			anim.SetBool ("grounded", false);
+
+			if (playerGroundCheck.isGrounded) {
+				anim.SetTrigger ("jump");
+				anim.ResetTrigger ("air_jump");
+			} else {
+				anim.SetTrigger ("air_jump");
+				anim.ResetTrigger ("jump");
+			}
+		}
+	}
+
+	public void ResetJumps() {
+		remainingJumps = numJumps;
+	}
+}
