4f76b306474e26f78c0fc33d5909046a5526b0ac
sprinaw1 <31804756+sprinaw1@users.noreply.github.com>
2017-09-30 22:13:32 -0400

    Add files via upload

---
+++ Assets/MoveCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    float speed = 10f;
+    float jump = 12f;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * Time.deltaTime * jump);
+
+   
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
