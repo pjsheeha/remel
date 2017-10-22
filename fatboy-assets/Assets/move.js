9bde838926a349538f0bf9967f5ba5893d4b881e
Constance Ye <prismacye@gmail.com>
2017-10-22 14:24:14 -0400

    enemy 4

---
+++ fatboy-assets/Assets/move.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+}
+
+function Update () {
+	var vec = Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	transform.position += vec * 2 * Time.deltaTime;
+
+}
+
+function OnCollisonEnter(collision: Collision){
+	Debug.Log("asdfadsfasdFadsf");
+}
