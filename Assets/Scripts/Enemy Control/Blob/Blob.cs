40f03f20656f8945553cd222ddde3bf9d7478223
Alexander Messam <alexmessam32@gmail.com>
2017-10-22 14:15:49 -0400

    Sound Manager

---
+++ Assets/Scripts/Enemy Control/Blob/Blob.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Remel.Player;
+
+public class Blob : MonoBehaviour {
+
+	[SerializeField]
+	protected float sizeChangeSpeed = 1f;
+	[SerializeField]
+	protected Vector2 sizeLimits = new Vector2(0.1f, 4f);
+
+	private SpriteRenderer sr;
+
+	private float h_Input;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h_Input = Input.GetAxis ("Horizontal");
+
+		ChangeSizeOnMovement (h_Input);
+	}
+
+	protected void ChangeSizeOnMovement(float horizontal) {
+		transform.localScale += Vector3.one * horizontal * sizeChangeSpeed * Time.deltaTime;
+	}
+}
